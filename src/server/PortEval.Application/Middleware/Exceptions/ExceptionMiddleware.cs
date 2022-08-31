54580b8ea8e170cca296696e31af72bb334417bb
Dmitry Simonov <dima.simonov@gmail.com>
2022-08-31 21:05:02 +0200

    refactor: display stack trace in log on API error

--- src/server/PortEval.Application/Middleware/Exceptions/ExceptionMiddleware.cs
+++ src/server/PortEval.Application/Middleware/Exceptions/ExceptionMiddleware.cs
@@ -31,7 +31,7 @@ namespace PortEval.Application.Middleware.Exceptions
             }
             catch (Exception ex)
             {
-                _logger.LogError($"{ex.Message}");
+                _logger.LogError($"{ex.StackTrace}");
                 await HandleExceptionAsync(context, ex);
             }
         }
