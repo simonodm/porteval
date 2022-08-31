f510bcfda8342aeef92aa02280457be76fa2fd9a
Dmitry Simonov <dima.simonov@gmail.com>
2022-08-31 21:12:10 +0200

    feat: refresh data button on front-end

--- src/web/src/components/Header.tsx
+++ src/web/src/components/Header.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { portEvalApi } from '../redux/api/portEvalApi';
 import './Header.css';
 
 export default function Header(): JSX.Element {
+    const dispatch = useDispatch();
+
     return (
         <nav className="navbar navbar-dark bg-dark p-0">
             <span id="app-logo">
                 <a className="navbar-brand" href="/">PortEval</a>
+            </span>
+            <span className="float-right mr-1">
+                <button className="btn btn-primary btn-sm" onClick={() => dispatch(portEvalApi.util.resetApiState())}>
+                    <i className="bi bi-arrow-clockwise"></i>
+                </button>
             </span>
         </nav>
     )
