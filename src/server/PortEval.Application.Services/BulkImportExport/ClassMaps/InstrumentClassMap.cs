14365a8391884bc8d47eca5360f2c9ed60d7f1af
Dmitry Simonov <dima.simonov@gmail.com>
2022-09-05 14:12:21 +0200

    fix: make instrument type case insensitive in CSV import

--- src/server/PortEval.Application.Services/BulkImportExport/ClassMaps/InstrumentClassMap.cs
+++ src/server/PortEval.Application.Services/BulkImportExport/ClassMaps/InstrumentClassMap.cs
@@ -14,6 +14,7 @@ namespace PortEval.Application.Services.BulkImportExport.ClassMaps
             Map(i => i.Name).Name("Name");
             Map(i => i.Exchange).Name("Exchange");
             Map(i => i.Type).Name("Type").TypeConverter(new EnumConverter(typeof(InstrumentType)));
+            Map(i => i.Type).TypeConverterOption.EnumIgnoreCase();
             Map(i => i.CurrencyCode).Name("Currency");
             Map(i => i.Note).Name("Note");
         }
