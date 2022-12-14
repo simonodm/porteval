8fed2bf09d1777e88ab546ec166b6d2887c0188c
Dmitry Simonov <dima.simonov@gmail.com>
2022-12-14 16:10:32 +0100

    fix: duplicate initial price fetch when creating an instrument

--- src/server/PortEval.Application.Services/BulkImportExport/InstrumentImportProcessor.cs
+++ src/server/PortEval.Application.Services/BulkImportExport/InstrumentImportProcessor.cs
@@ -1,10 +1,6 @@
-﻿using Hangfire;
-using PortEval.Application.Models.DTOs;
+﻿using PortEval.Application.Models.DTOs;
 using PortEval.Application.Models.Validators;
 using PortEval.Application.Services.Interfaces;
-using PortEval.Application.Services.Interfaces.BackgroundJobs;
-using PortEval.Domain.Models.Entities;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace PortEval.Application.Services.BulkImportExport
@@ -12,22 +8,10 @@ namespace PortEval.Application.Services.BulkImportExport
     public class InstrumentImportProcessor : ImportProcessor<InstrumentDto, InstrumentDtoValidator>
     {
         private readonly IInstrumentService _instrumentService;
-        private readonly IBackgroundJobClient _jobClient;
-        private readonly List<Instrument> _newInstruments;
 
-        public InstrumentImportProcessor(IInstrumentService instrumentService, IBackgroundJobClient jobClient) : base()
+        public InstrumentImportProcessor(IInstrumentService instrumentService) : base()
         {
             _instrumentService = instrumentService;
-            _jobClient = jobClient;
-            _newInstruments = new List<Instrument>();
-
-            OnImportFinish = () =>
-            {
-                foreach (var instrument in _newInstruments)
-                {
-                    _jobClient.Enqueue<IInitialPriceFetchJob>(job => job.Run(instrument.Id));
-                }
-            };
         }
 
         protected override async Task<ProcessedRowErrorLogEntry<InstrumentDto>> ProcessItem(InstrumentDto row)
@@ -41,7 +25,6 @@ namespace PortEval.Application.Services.BulkImportExport
             else
             {
                 var instrument = await _instrumentService.CreateInstrumentAsync(row);
-                _newInstruments.Add(instrument);
                 logEntry.Data.Id = instrument.Id;
             }
 
