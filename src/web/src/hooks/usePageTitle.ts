472c616158182452a7186fd1cac9c5f4e2c97b60
Dmitry Simonov <dmitry@dmitrysimonov.com>
2023-05-02 13:36:22 +0200

    fix(web): revert page title on PageHeading unmount

--- src/web/src/hooks/usePageTitle.ts
+++ src/web/src/hooks/usePageTitle.ts
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 /**
  * A hook to update page title.
@@ -6,9 +6,16 @@ import { useEffect } from 'react';
  * @category Hooks
  * @param title Page title to set.
  */
-function usePageTitle(title: string): void {
+function usePageTitle(title: string | undefined | null): void {
+    const [prevTitle, setPrevTitle] = useState(document.title);
+
     useEffect(() => {
+        setPrevTitle(document.title);
         document.title = title ? `PortEval - ${title}` : 'PortEval';
+
+        return () => {
+            document.title = prevTitle;
+        }
     }, [title]);
 }
 
